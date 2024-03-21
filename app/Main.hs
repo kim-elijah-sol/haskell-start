7d1539eb30c7104a64c817b7b42779fb75815811
kim-elijah-sol <kimelijahsol@gmail.com>
2024-03-21 15:32:53 +0900

    Int 데이터 다루기 + putStrLn

--- app/Main.hs
+++ app/Main.hs
@@ -1,2 +1,13 @@
+-- :: 는 타입을 나타내는 키워드
+add :: Int -> Int -> Int
+add a b = a + b
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    -- putStrLn은 문자열을 출력하는 함수
+    -- $ 는 함수를 호출하는 연산자
+    -- ++ 는 문자열을 이어붙이는 연산자
+    -- show는 값을 문자열로 변환하는 함수
+    putStrLn $ "1 + 2 = " ++ show (1 + 2)
+    putStrLn $ "3 + 4 = " ++ show (add 3 4)
+    putStrLn $ "5 + 10 = " ++ show (add 5 10)
